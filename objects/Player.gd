e900ab01d1ee15622749dfa56c1a6c6303ef3101
Rodrigo Alfonso <rodri042@gmail.com>
2020-02-10 04:46:15 -0300

    Preparing player's scene for collisions

--- objects/Player.gd
+++ objects/Player.gd
@@ -1,10 +1,25 @@
 extends Area2D
 
+signal hit
+
 export var speed = 400  # How fast the player will move (pixels/sec).
 var screen_size  # Size of the game window.
 
+func start(new_position):
+	position = new_position
+	show()
+	$CollisionShape2D.disabled = false
+
 func _ready():
+	# hide()
 	screen_size = get_viewport_rect().size
+
+func _on_body_entered(body):
+	hide()  # Player disappears after being hit.
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
+	# ^ Disabling the area’s collision shape can cause an error if it happens
+	# in the middle of the engine’s collision processing; hence the `deferred`.
 
 func _process(delta):
 	var velocity = _get_velocity()
